67eafd227683fae92da66ec12ddc9b4d04d47355
rafaelvelosoAZ <rafa.v_s@hotmail.com>
2021-08-10 18:20:18 -0300

    news

--- terraform/projetos/stacks/backend/backend.tf
+++ terraform/projetos/stacks/backend/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "terraform-resources-eastus"
     storage_account_name = "ts6b0eeastusdev"
     container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
+    key                  = "terra.tfstate"
     subscription_id      = "27879557-aacc-4cbf-b226-16d05e5c026b"
   }
 }
