5697d9a9e00cbe0572269d8d794a223f232cc652
rafaelvelosoAZ <rafa.v_s@hotmail.com>
2021-08-10 14:21:42 -0300

    new

--- terraform/projetos/stacks/backend/backend.tf
+++ terraform/projetos/stacks/backend/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-new-one"
-    storage_account_name = "tfnewone"
+    resource_group_name  = "terraform-resources-eastus"
+    storage_account_name = "ts6b0eeastusdev"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
     subscription_id      = "27879557-aacc-4cbf-b226-16d05e5c026b"
