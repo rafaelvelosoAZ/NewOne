f1f7f7883dfb2f44de40bacacc8aa534cfd78a99
rafaelvelosoAZ <rafa.v_s@hotmail.com>
2021-08-11 18:39:53 -0300

    news

---
+++ backend/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "newone"
+    storage_account_name = "newonestg"
+    container_name       = "terraform-states"
+    key                  = "tfstate"
+    subscription_id      = "27879557-aacc-4cbf-b226-16d05e5c026b"
+  }
+}
